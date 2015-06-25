c108179b0ca7d37adfd972399db4cb56461ed112
Hugo Wainwright <hwainwright@gmx.com>
2015-06-25 01:21:56 +0100

    - imported cave rooms from tiled - created new sublcass of DefaultRoomBehaviour which calculates room area   from attached tiled map

---
+++ Assets/Scripts/Room/TiledRoomBehaviour.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Assets.Scripts.Room {
+    using DefaultRoom;
+    using Tiled2Unity;
+    using UnityEngine;
+
+    public class TiledRoomBehaviour : DefaultRoomBehaviour {
+
+        public override Rect Area {
+            get {
+                var tiledMap = GetComponentInChildren<TiledMap>();
+
+                return new Rect(tiledMap.transform.position.x,
+                    tiledMap.transform.position.y - tiledMap.NumTilesHigh,
+                    tiledMap.NumTilesWide,
+                    tiledMap.NumTilesHigh);
+            }
+        }
+    }
+}
