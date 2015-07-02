3f0084c894fefa46677328e621f71bfe8652aa3c
Hugo Wainwright <hwainwright@gmx.com>
2015-07-02 23:08:06 +0100

    -- added shadow -- adjusted sprite sorting order to take into account parent sprite ordering

--- Assets/Scripts/SpriteSortingOrderAdjusterBehaviour.cs
+++ Assets/Scripts/SpriteSortingOrderAdjusterBehaviour.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
 
 namespace Assets.Scripts {
-    
+
     [RequireComponent(typeof (SpriteRenderer))]
     public class SpriteSortingOrderAdjusterBehaviour : MonoBehaviour {
 
+        private SpriteSortingOrderAdjusterBehaviour _spriteSortingOrderAdjuster;
+        private SpriteRenderer _spriteRenderer;
+
+        public void Awake() {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _spriteSortingOrderAdjuster = transform.parent != null
+                ? transform.parent.GetComponent<SpriteSortingOrderAdjusterBehaviour>() ?? this
+                : this;
+        }
+
         public void Update() {
-            var spriteRenderer = GetComponent<SpriteRenderer>();
-            spriteRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y * 100);
+            if (_spriteSortingOrderAdjuster == this) {
+                _spriteRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y * 1000);
+            } else {
+                _spriteRenderer.sortingOrder = _spriteSortingOrderAdjuster._spriteRenderer.sortingOrder - 1;
+            }
         }
     }
 }
